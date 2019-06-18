70ca4a399b53bd8a7a0f12d15bcfea0c82c08b3e
bryandejong <bdjong1@avans.nl>
2019-06-18 15:53:09 +0200

    Generate monster from user input

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ import Monster, { fur, type, arm, color } from './monster.js';
 
 function generate(){
     let name = document.getElementById("confName").value;
-    let name = document.getElementById("confType").value;
-    let name = document.getElementById("confArmType").value;
-    let name = document.getElementById("confFurType").value;
-    let name = document.getElementById("confArm").value;
-    let name = document.getElementById("confLegs").value;
-    let monster = new Monster(name, type.AIR, 5, 5, 5, true, true, fur.SLIME, color.PURPLE, "test");
+    let type = document.getElementById("confType").value;
+    let armType = document.getElementById("confArmType").value;
+    let furType = document.getElementById("confFurType").value;
+    let arms = document.getElementById("confArms").value;
+    let legs = document.getElementById("confLegs").value;
+    let eyes = document.getElementById("confEyes").value;
+    let monster = new Monster(name, type, furType, color.PURPLE, armType, arms, legs, eyes, false, false, "No image");
     monster.printAll();
 };
 
