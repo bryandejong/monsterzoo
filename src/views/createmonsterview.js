66a6540f53b119b43aa4f57f7162869ecf1295cc
mussaf <38488831+mussaf@users.noreply.github.com>
2019-06-20 15:27:34 +0200

    add eventlistener

--- src/views/createmonsterview.js
+++ src/views/createmonsterview.js
@@ -7,17 +7,24 @@ export default class CreateMonsterView {
     }
     
     populateSelectFields(){
+
         let confType = document.getElementById("confType")
-      
+        confType.addEventListener("change", function(){
+            console.log("changed " + document.getElementById("confType").value )
+        })
+
         for(let item in type){
             let optType = document.createElement('option');
             optType.value= type[item];
             optType.innerHTML = type[item];
             confType.appendChild(optType);
         };
-        
+
         let confArmType = document.getElementById("confArmType")
-      
+        confArmType.addEventListener("change", function(){
+            console.log("changed " + document.getElementById("confArmType").value )
+        });
+
         for(let item in arm){
             let optArmType = document.createElement('option');
             optArmType.value= arm[item];
@@ -26,6 +33,9 @@ export default class CreateMonsterView {
         };
         
         let confFurType = document.getElementById("confFurType")
+        confFurType.addEventListener("change", function(){
+            console.log("changed " + document.getElementById("confFurType").value )
+        })
       
         for(let item in fur){
             let optFurType = document.createElement('option');
@@ -33,6 +43,12 @@ export default class CreateMonsterView {
             optFurType.innerHTML = fur[item];
             confFurType.appendChild(optFurType);
         };
+    }
+
+    checkSelectField(value){
+        if(value == "Water"){
+            
+        }
     }
     
 }
