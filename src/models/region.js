af87a23741c02c3a76d34a3ec04df7df35239b35
bryandejong <bdjong1@avans.nl>
2019-06-21 19:58:57 +0200

    Dynamic region loading from JSON, bg and obstacle style

---
+++ src/models/region.js
@@ -0,0 +1,8 @@
+export default class Region {
+
+    constructor(name, bgClass, obstacleImgUrl) {
+        this.name = name;
+        this.bgClass = bgClass;
+        this.obstacleImgUrl = obstacleImgUrl;
+    }
+}
