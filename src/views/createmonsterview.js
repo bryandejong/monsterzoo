02dd91f1d7fb0b3b19555876ca0545ea9deca20d
mussaf <38488831+mussaf@users.noreply.github.com>
2019-06-23 02:02:07 +0200

    beetje lelijk gefixt values

--- src/views/createmonsterview.js
+++ src/views/createmonsterview.js
@@ -13,13 +13,13 @@ export default class CreateMonsterView {
     populateSelectFields(){
         var self = this;
 
-        let confArray = new Array("confArmType","confFurType","confType", "numberOfArms", "numberOfLegs", "numberOfEyes");
+        let confArray = new Array("confArmType","confFurType","confType", "numberOfArms", "numberOfLegs", "numberOfEyes", "colors");
         
         confArray.forEach(function(conf){
             let el = document.getElementById(conf)
             el.addEventListener("change", function(){
                 
-                let elementArray =  self.controller.checkSelectField(document.getElementById("confType").value,  document.getElementById("confFurType").value);
+                let elementArray =  self.controller.checkSelectField(document.getElementById("confType").value, document.getElementById("confArmType").value,  document.getElementById("confFurType").value, document.getElementById("numberOfArms").value, document.getElementById("numberOfLegs").value, document.getElementById("numberOfEyes").value, document.getElementById("colors").value);
                 self.removePopulatedFields(confArray);
                 self.removeGeneratedMonster();
                 self.rePopulateField(elementArray)
@@ -35,12 +35,13 @@ export default class CreateMonsterView {
             optType.innerHTML = array[1][item];
             confArmType.appendChild(optType);
         };
-
+        debugger;
         for(let item in array[4]){
             let optType = document.createElement('option');
             optType.value = array[4][item];
             optType.innerHTML = array[4][item];
             confFurType.appendChild(optType);
+            
         };
 
         for(let item in array[0]){
@@ -63,11 +64,18 @@ export default class CreateMonsterView {
             optType.innerHTML = array[3][item];
             numberOfEyes.appendChild(optType);
         };
+        debugger;
+        for(let item in array[7]){
+            let optType = document.createElement('option');
+            optType.value = array[7][item];
+            optType.innerHTML = array[7][item];
+            colors.appendChild(optType);
+        };
     }
 
     firstPopulate(){
 
-        let firstPopulateArray = new Array(["Water", "Fire", "Air", "Earth"], ["Tentacles", "Fins"], ["Scales", "Slime"],[0,1,2,3,4,5,6,7,8],[0,1,2,3,4],[0,1,2,3,4,5,6,7,8])
+        let firstPopulateArray = new Array(["Water", "Fire", "Air", "Earth"], ["Tentacles", "Fins"], ["Scales", "Slime"],[0,1,2,3,4,5,6,7,8],[0,1,2,3,4],[0,1,2,3,4,5,6,7,8], [color.BLUE, color.RED, color.GREEN])
 
 
         for(let item in firstPopulateArray[0]){
@@ -112,6 +120,12 @@ export default class CreateMonsterView {
             numberOfEyes.appendChild(optType);
         };
 
+        for(let item in firstPopulateArray[6]){
+            let optType = document.createElement('option');
+            optType.value = firstPopulateArray[6][item];
+            optType.innerHTML = firstPopulateArray[6][item];
+            colors.appendChild(optType);
+        };
         
     }
 
